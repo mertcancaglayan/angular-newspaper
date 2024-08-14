0aa339501d618728dc6c4d1c60867edc6b787759
M.can <caglayan.mertcan37@gmail.com>
2024-08-14 13:37:46 +0300

    Initial commit - Set up Angular project structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,7 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: ``,
 })
 export class AppComponent {
   title = 'angular-newspaper';
